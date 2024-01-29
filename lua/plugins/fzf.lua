c9e9eb9e2af241fc9926b88a6f047ec4f69789e7
Lars Michelsen <lm@checkmk.com>
2024-01-29 12:50:50 +0100

    Remove old fzf dependency + open files from fzf-lua in tabs

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -12,35 +12,11 @@ end
 
 return {
     {
-        -- https://github.com/junegunn/fzf/
-        "junegunn/fzf",
-        lazy = false,
-        build = "./install --bin",
-        init = function()
-            --vim.opt.runtimepath:append(',~/.fzf')
-            -- Enable CTRL-N and CTRL-P for history forward/backward search in CTRL-A mode
-            vim.g.fzf_history_dir = "~/.local/share/fzf-history"
-            --vim.g.fzf_layout = { down = '40%' }
-
-            --vim.g.fzf_action = {
-            --  ['ctrl-m'] = 'tabedit',
-            --  ['ctrl-t'] = 'tabedit',
-            --  ['ctrl-h'] = 'botright split',
-            --  ['ctrl-v'] = 'vertical botright split',
-            --}
-
-            -- TODO Investigate howto clean this up
-            vim.cmd([[
-            command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, '--smart-case -W 200 --python --markdown --yaml --cpp --ts --js --php --sass --make --rust --bazel --follow --ignore ".mypy_cache" --ignore "*_min.js" --ignore jquery --ignore agents/windows/chroot --ignore node_modules --ignore omd/packages/python-modules/destdir --ignore "livestatus" --ignore "skel/opt/omd" --color-path="0;33"', <bang>0)
-            ]])
-        end,
-    },
-    {
         "ibhagwan/fzf-lua",
         -- optional for icon support
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            -- calling `setup` is optional for customization
+            local actions = require("fzf-lua.actions")
             require("fzf-lua").setup({
                 winopts = {
                     height = 0.95,
@@ -50,6 +26,14 @@ return {
                 },
                 files = {
                     rg_opts = '--smart-case -g "!{.git,node_modules,.mypy_cache,swagger-ui-3}/*" -g !redoc.standalone.js -g !Pipfile.lock',
+                },
+                actions = {
+                    files = {
+                        ["enter"] = actions.file_tabedit,
+                        ["ctrl-s"] = actions.file_split,
+                        ["ctrl-v"] = actions.file_vsplit,
+                        ["ctrl-t"] = actions.file_tabedit,
+                    },
                 },
             })
         end,
