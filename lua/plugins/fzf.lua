a8af9992fb3fb33068462f21efd13703be91f40b
Lars Michelsen <lm@checkmk.com>
2024-01-29 09:56:16 +0100

    Fix format

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -19,21 +19,21 @@ return {
         init = function()
             --vim.opt.runtimepath:append(',~/.fzf')
             -- Enable CTRL-N and CTRL-P for history forward/backward search in CTRL-A mode
-            vim.g.fzf_history_dir = '~/.local/share/fzf-history'
+            vim.g.fzf_history_dir = "~/.local/share/fzf-history"
             --vim.g.fzf_layout = { down = '40%' }
-            
+
             --vim.g.fzf_action = {
             --  ['ctrl-m'] = 'tabedit',
             --  ['ctrl-t'] = 'tabedit',
             --  ['ctrl-h'] = 'botright split',
             --  ['ctrl-v'] = 'vertical botright split',
             --}
-            
+
             -- TODO Investigate howto clean this up
             vim.cmd([[
             command! -bang -nargs=* Ag call fzf#vim#ag(<q-args>, '--smart-case -W 200 --python --markdown --yaml --cpp --ts --js --php --sass --make --rust --bazel --follow --ignore ".mypy_cache" --ignore "*_min.js" --ignore jquery --ignore agents/windows/chroot --ignore node_modules --ignore omd/packages/python-modules/destdir --ignore "livestatus" --ignore "skel/opt/omd" --color-path="0;33"', <bang>0)
             ]])
-        end
+        end,
     },
     {
         "ibhagwan/fzf-lua",
@@ -48,20 +48,20 @@ return {
                     row = 0.30,
                     col = 0.30,
                 },
-		files = {
-	            rg_opts = '--smart-case -g "!{.git,node_modules,.mypy_cache,swagger-ui-3}/*" -g !redoc.standalone.js -g !Pipfile.lock',
-		},
+                files = {
+                    rg_opts = '--smart-case -g "!{.git,node_modules,.mypy_cache,swagger-ui-3}/*" -g !redoc.standalone.js -g !Pipfile.lock',
+                },
             })
         end,
         init = function()
-            map('n', '<C-t>', "<cmd>lua require('fzf-lua').files()<CR>")
-            map('n', '<C-p>', "<cmd>lua require('fzf-lua').git_files()<CR>")
-            map('n', '<C-l>', "<cmd>lua require('fzf-lua').git_bcommits()<CR>")
-            map('n', '<C-h>', "<cmd>lua require('fzf-lua').oldfiles()<CR>")
-            map('n', '<C-a>', '<cmd>lua require("fzf-lua").live_grep_native()<CR>')
+            map("n", "<C-t>", "<cmd>lua require('fzf-lua').files()<CR>")
+            map("n", "<C-p>", "<cmd>lua require('fzf-lua').git_files()<CR>")
+            map("n", "<C-l>", "<cmd>lua require('fzf-lua').git_bcommits()<CR>")
+            map("n", "<C-h>", "<cmd>lua require('fzf-lua').oldfiles()<CR>")
+            map("n", "<C-a>", '<cmd>lua require("fzf-lua").live_grep_native()<CR>')
             --map('n', '<C-s>', ':Ag!<CR>')
             -- Immediately search for the word under the cursor in a new tab
-            map('n', '<Leader>ag', "<cmd>lua require('fzf-lua').grep_cword()<CR>")
-        end
+            map("n", "<Leader>ag", "<cmd>lua require('fzf-lua').grep_cword()<CR>")
+        end,
     },
 }
