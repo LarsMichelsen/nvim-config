96c39d792824eac4bef40b934b38ca2367931fb4
Lars Michelsen <lm@checkmk.com>
2024-06-03 13:48:43 +0200

    Fix shellcheck

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -476,6 +476,13 @@ return {
                 yaml = { "yamllint" },
             }
 
+            lint.linters.shellcheck.args = {
+                "--format",
+                "json",
+                "-x",
+                "-",
+            }
+
             local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
             vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
