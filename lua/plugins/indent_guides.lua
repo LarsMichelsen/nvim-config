78f6666687704f8585a6d3924d94e525b53aa5e2
Lars Michelsen <lm@checkmk.com>
2024-09-08 20:47:34 +0200

    Simplify config (fix dark/light issues)

--- lua/plugins/indent_guides.lua
+++ lua/plugins/indent_guides.lua
@@ -4,15 +4,5 @@ return {
     init = function()
         vim.g.indent_guides_enable_on_vim_startup = 1
         vim.g.indent_guides_color_change_percent = 5
-        vim.g.indent_guides_auto_colors = 0
-
-        vim.api.nvim_create_autocmd({ "VimEnter", "Colorscheme" }, {
-            pattern = "*",
-            command = ":hi IndentGuidesOdd  guibg=#06303b ctermbg=3",
-        })
-        vim.api.nvim_create_autocmd({ "VimEnter", "Colorscheme" }, {
-            pattern = "*",
-            command = ":hi IndentGuidesEven  guibg=#073642 ctermbg=4",
-        })
     end,
 }
