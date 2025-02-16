d3879587a4752312fccfe2b66d7166ab02e09518
Lars Michelsen <lm@checkmk.com>
2025-02-16 16:31:45 +0100

    Improve diffview config

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,6 +1,15 @@
 return {
     "sindrets/diffview.nvim",
     config = function()
-        require("diffview").setup({})
+        require("diffview").setup({
+            view = {
+                merge_tool = {
+                    -- Config for conflicted files in diff views during a merge or rebase.
+                    layout = "diff3_mixed",
+                    disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+                    winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+                },
+            },
+        })
     end,
 }
