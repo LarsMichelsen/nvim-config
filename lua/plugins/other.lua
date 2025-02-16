deed631172c00522e1f2d169f67ef43706d2b256
Lars Michelsen <lm@checkmk.com>
2025-02-16 14:54:32 +0100

    Try diffview and other

---
+++ lua/plugins/other.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        "rgroli/other.nvim",
+        config = function()
+            require("other-nvim").setup({
+                mappings = {
+                    -- Python test file mapping
+                    {
+                        pattern = "/home/lm/git/([^/]*)/(.*)/(.*).py$",
+                        target = "/home/lm/git/%1/tests/unit/%2/test_%3.py",
+                        transformer = "lowercase",
+                    },
+                },
+                style = {
+                    -- How the plugin paints its window borders
+                    -- Allowed values are none, single, double, rounded, solid and shadow
+                    border = "solid",
+
+                    -- Column seperator for the window
+                    seperator = "|",
+
+                    -- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
+                    width = 0.7,
+
+                    -- min height in rows.
+                    -- when more columns are needed this value is extended automatically
+                    minHeight = 2,
+                },
+            })
+        end,
+    },
+}
