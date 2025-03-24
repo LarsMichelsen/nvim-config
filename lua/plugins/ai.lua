7236866f2f3a975aa53b2decd0a95b95f98474fa
Lars Michelsen <lm@checkmk.com>
2025-03-24 20:46:59 +0100

    Simplify exclusion, use new should_attach feature of copilot plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -28,6 +28,43 @@ return {
                     cvs = false,
                     ["."] = false,
                 },
+                should_attach = function(_, bufname)
+                    local cwd = vim.fn.getcwd() .. "/"
+
+                    local enable_ai = false
+
+                    -- see on matching doc
+                    -- https://www.lua.org/manual/5.3/manual.html#pdf-string.match
+                    -- https://www.lua.org/manual/5.3/manual.html#6.4.1
+                    local home_dir = vim.fn.expand("~")
+                    local enable_patterns = {
+                        home_dir .. "/.config/nvim/",
+                        home_dir .. "/git/checkmk/",
+                        home_dir .. "/git/checkmk%-%d%.%d%.%d/",
+                        home_dir .. "/git/cma/",
+                        home_dir .. "/git/cma-%d%.%d/",
+                        home_dir .. "/git/lm/nagvis/",
+                        home_dir .. "/git/cmk-app/",
+                        home_dir .. "/git/gerrit/",
+                    }
+                    for _, pattern in ipairs(enable_patterns) do
+                        if (bufname == "" and cwd:match("^" .. pattern)) or bufname:match("^" .. pattern) then
+                            enable_ai = true
+                            break
+                        end
+                    end
+
+                    -- Disable copilot on buffers bigger than 300kb
+                    -- (https://til.codeinthehole.com/posts/how-to-automatically-disable-github-copilot-in-vim-when-editing-large-files/)
+                    if bufname ~= "" then
+                        local fsize = vim.fn.getfsize(bufname)
+                        if fsize > 300000 or fsize == -2 then
+                            enable_ai = false
+                        end
+                    end
+
+                    return enable_ai
+                end,
             })
 
             -- hide copilot suggestions when cmp menu is open
@@ -54,66 +91,6 @@ return {
             -- set log level to DEBUG and have a look at the lsp log, e.g. with:
             -- tail -f $HOME/.local/state/nvim/lsp.log | grep -i copilot
             --vim.lsp.set_log_level("DEBUG")
-
-            -- Enable on specific files / folders
-            -- Inspiration taken from here: https://github.com/zbirenbaum/copilot.lua/issues/74
-            -- I am not 100% sure whether this approach is 100% safe because the Lsp is attached
-            -- and detached again short after to my understanding. But it seems to work.
-            local augroup = vim.api.nvim_create_augroup("copilot-disable-patterns", { clear = true })
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = augroup,
-                pattern = "*",
-                callback = function(args)
-                    -- Enables copilot only for files in allowed working directories
-                    --
-                    -- If the buffer has a name (file associated), the path is matched
-                    -- If not, we check if CWD is an allowed directory
-                    --
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if client.name ~= "copilot" then
-                        return
-                    end
-
-                    local bufname = vim.api.nvim_buf_get_name(0)
-                    local cwd = vim.fn.getcwd() .. "/"
-
-                    local enable_ai = false
-
-                    -- see on matching doc
-                    -- https://www.lua.org/manual/5.3/manual.html#pdf-string.match
-                    -- https://www.lua.org/manual/5.3/manual.html#6.4.1
-                    local home_dir = vim.fn.expand("~")
-                    local enable_patterns = {
-                        home_dir .. "/.config/nvim/",
-                        home_dir .. "/git/checkmk/",
-                        home_dir .. "/git/checkmk%-%d%.%d%.%d/",
-                        home_dir .. "/git/cma/",
-                        home_dir .. "/git/cma-%d%.%d/",
-                        home_dir .. "/git/lm/nagvis/",
-                        home_dir .. "/git/cmk-app/",
-                    }
-                    for _, pattern in ipairs(enable_patterns) do
-                        if (bufname == "" and cwd:match("^" .. pattern)) or bufname:match("^" .. pattern) then
-                            enable_ai = true
-                            break
-                        end
-                    end
-
-                    -- Disable copilot on buffers bigger than 100kb
-                    -- (https://til.codeinthehole.com/posts/how-to-automatically-disable-github-copilot-in-vim-when-editing-large-files/)
-                    if bufname ~= "" then
-                        local fsize = vim.fn.getfsize(bufname)
-                        if fsize > 100000 or fsize == -2 then
-                            enable_ai = false
-                        end
-                    end
-
-                    vim.defer_fn(function()
-                        -- print("Copilot " .. (enable_ai and "attach" or "detach"))
-                        vim.cmd("silent Copilot " .. (enable_ai and "attach" or "detach"))
-                    end, 0)
-                end,
-            })
         end,
     },
     {
