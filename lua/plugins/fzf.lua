97ccf7aa8a6a8505da65cb0fbf55e4b17519d01f
Lars Michelsen <lm@checkmk.com>
2025-09-28 12:16:08 +0200

    Move from fzf-lua to telescope

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,54 +2,56 @@
 -- Fuzzy find using fzf (Triggered using CTRL-A)
 --
 
-return {
-    {
-        "ibhagwan/fzf-lua",
-        -- optional for icon support
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            local actions = require("fzf-lua.actions")
-            require("fzf-lua").setup({
-                winopts = {
-                    height = 0.95,
-                    width = 0.95,
-                    row = 0.30,
-                    col = 0.30,
-                    preview = {
-                        horizontal = "right:40%",
-                    },
-                },
-                grep = {
-                    rg_opts = '--line-number --column --smart-case -g "!{.git,node_modules,.mypy_cache,swagger-ui-3,tests/qa-test-data,third_party}/*" -g !redoc.standalone.js -g "!*.lock" --type-not svg --type-not po',
-                },
-                files = {
-                    rg_opts = '--line-number --column --smart-case -g "!{.git,node_modules,.mypy_cache,swagger-ui-3,tests/qa-test-data}/*" -g "!third_party/*" -g !redoc.standalone.js -g "!*.lock" --type-not svg --type-not po',
-                },
-                oldfiles = {
-                    include_current_session = true,
-                },
-                actions = {
-                    files = {
-                        ["enter"] = actions.file_tabedit,
-                        ["ctrl-s"] = actions.file_split,
-                        ["ctrl-v"] = actions.file_vsplit,
-                        ["ctrl-t"] = actions.file_tabedit,
-                    },
-                },
-            })
-        end,
-        init = function()
-            local wk = require("which-key")
-            wk.add({
-                { "<C-a>", "<cmd>lua require('fzf-lua').live_grep_native()<CR>", desc = "Find in files" },
-                { "<C-s>", "<cmd>lua require('fzf-lua').resume()<CR>", desc = "Resume last" },
-                { "<C-b>", "<cmd>lua require('fzf-lua').buffers()<CR>", desc = "Find buffers" },
-                { "<C-h>", "<cmd>lua require('fzf-lua').oldfiles()<CR>", desc = "Find file history" },
-                { "<C-l>", "<cmd>lua require('fzf-lua').git_bcommits()<CR>", desc = "Find git commits" },
-                { "<C-p>", "<cmd>lua require('fzf-lua').git_files()<CR>", desc = "Find git files" },
-                { "<C-t>", "<cmd>lua require('fzf-lua').files()<CR>", desc = "Find files" },
-                { "<leader>wf", "<cmd>lua require('fzf-lua').grep_cword()<CR>", desc = "Find word under cursor" },
-            })
-        end,
-    },
-}
+return {}
+--return {
+--    {
+--        "ibhagwan/fzf-lua",
+--        -- optional for icon support
+--        dependencies = { "nvim-tree/nvim-web-devicons" },
+--        config = function()
+--            local actions = require("fzf-lua.actions")
+--            require("fzf-lua").setup({
+--                "hide",
+--                winopts = {
+--                    height = 0.95,
+--                    width = 0.95,
+--                    row = 0.30,
+--                    col = 0.30,
+--                    preview = {
+--                        horizontal = "right:40%",
+--                    },
+--                },
+--                grep = {
+--                    rg_opts = '--line-number --column --smart-case -g "!{.venv,.git,node_modules,.mypy_cache,swagger-ui-3,tests/qa-test-data,third_party}/*" -g "!go.sum" -g "!*.lock" --type-not svg --type-not po',
+--                },
+--                files = {
+--                    rg_opts = '--line-number --column --smart-case -g "!{.venv,.git,node_modules,.mypy_cache,swagger-ui-3,tests/qa-test-data}/*" -g "!go.sum" -g "!third_party/*" -g "!*.lock" --type-not svg --type-not po',
+--                },
+--                oldfiles = {
+--                    include_current_session = true,
+--                },
+--                actions = {
+--                    files = {
+--                        ["enter"] = actions.file_tabedit,
+--                        ["ctrl-s"] = actions.file_split,
+--                        ["ctrl-v"] = actions.file_vsplit,
+--                        ["ctrl-t"] = actions.file_tabedit,
+--                    },
+--                },
+--            })
+--        end,
+--        init = function()
+--            local wk = require("which-key")
+--            wk.add({
+--                { "<C-a>", "<cmd>lua require('fzf-lua').live_grep_native()<CR>", desc = "Find in files" },
+--                { "<C-s>", "<cmd>lua require('fzf-lua').resume()<CR>", desc = "Resume last" },
+--                { "<C-b>", "<cmd>lua require('fzf-lua').buffers()<CR>", desc = "Find buffers" },
+--                { "<C-h>", "<cmd>lua require('fzf-lua').oldfiles()<CR>", desc = "Find file history" },
+--                { "<C-l>", "<cmd>lua require('fzf-lua').git_bcommits()<CR>", desc = "Find git commits" },
+--                { "<C-p>", "<cmd>lua require('fzf-lua').git_files()<CR>", desc = "Find git files" },
+--                { "<C-t>", "<cmd>lua require('fzf-lua').files()<CR>", desc = "Find files" },
+--                { "<leader>wf", "<cmd>lua require('fzf-lua').grep_cword()<CR>", desc = "Find word under cursor" },
+--            })
+--        end,
+--    },
+--}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,46 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        cmd = "Telescope",
+        version = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                config = function()
+                    require("telescope").load_extension("fzf")
+                end,
+            },
+        },
+        init = function()
+            local wk = require("which-key")
+            local builtin = require("telescope.builtin")
+            wk.add({
+                { "<C-a>", builtin.live_grep, desc = "Live grep" },
+                { "<C-s>", builtin.resume, desc = "Resume last search" },
+                { "<C-b>", builtin.buffers, desc = "Find buffers" },
+                { "<C-h>", builtin.oldfiles, desc = "Recent files" },
+                { "<C-l>", builtin.git_bcommits, desc = "Find git commits" },
+                { "<C-p>", builtin.git_files, desc = "Find git files" },
+                { "<C-t>", builtin.find_files, desc = "Find files" },
+                { "<leader>wf", builtin.lsp_references, desc = "Find word under cursor" },
+            })
+        end,
+        opts = function()
+            local actions = require("telescope.actions")
+            return {
+                defaults = {
+                    layout_strategy = "horizontal",
+                    layout_config = {
+                        horizontal = {
+                            width = 0.98,
+                            height = 0.98,
+                            preview_height = 0.5,
+                        },
+                    },
+                },
+            }
+        end,
+    },
+}
