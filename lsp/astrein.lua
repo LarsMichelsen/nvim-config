daf4a33e78bf03d1cd8d99f192cfec7542ec13df
Lars Michelsen <lars.michelsen@checkmk.com>
2026-07-22 20:31:19 +0200

    Start astrein conditionally

--- lsp/astrein.lua
+++ lsp/astrein.lua
@@ -8,8 +8,13 @@ return {
         "/home/lm/git/checkmk",
     },
     filetypes = { "python" },
-    root_markers = {
-        ".git",
-        "pyproject.toml",
-    },
+    -- Only start in repos that actually ship the astrein Bazel target
+    -- (i.e. the checkmk repo); other repos are not Bazel workspaces and
+    -- `bazel run` would fail there.
+    root_dir = function(bufnr, on_dir)
+        local root = vim.fs.root(bufnr, "MODULE.bazel")
+        if root and vim.uv.fs_stat(root .. "/packages/cmk-astrein/BUILD") then
+            on_dir(root)
+        end
+    end,
 }
